f97185a761d7b818dcd5416a7a0dd48cd5bbc414
Rameshbo2580 <rameshbohora4578@gmail.com>
2026-01-04 20:23:27 +0545

    Use a loop to analyze numbers as many times as the user requests

--- numberAnalysis.java
+++ numberAnalysis.java
@@ -3,25 +3,28 @@ import java.util.Scanner;
 public class numberAnalysis {
    public static void main(String[]arg) {
        Scanner sc = new Scanner(System.in);
-       System.out.println("Enter the number!");
-       int number = sc.nextInt();
-       if (number % 2 == 0){
-           System.out.println("Its 'Even' number");
-       }else{
-           System.out.println("This number is 'Odd'");
-       }
-       if (number > 100) {
-           System.out.println("Its Greater than 100 ");
-       } else{
-           System.out.println("This number is less than 100");
-       }
-       if (number < 0 ){
-           System.out.println("Number is 'negative'");
-       }else if (number > 0 ){
-           System.out.println("Number is 'Positive' ");
-       }
-       else{
-           System.out.println("Number is 'Zero'");
+       System.out.println("How many time you want to analysis the number?");
+       int times = sc.nextInt();;
+       for (int i = 0; i <= times;i++) {
+           System.out.println("Enter the numbers!!!");
+           int number = sc.nextInt();
+           if (number % 2 == 0) {
+               System.out.println("Its 'Even' number");
+           } else {
+               System.out.println("This number is 'Odd'");
+           }
+           if (number > 100) {
+               System.out.println("Its Greater than 100 ");
+           } else {
+               System.out.println("This number is less than 100");
+           }
+           if (number < 0) {
+               System.out.println("Number is 'negative'");
+           } else if (number > 0) {
+               System.out.println("Number is 'Positive' ");
+           } else {
+               System.out.println("Number is 'Zero'");
+           }
        }
         sc.close();
    }
