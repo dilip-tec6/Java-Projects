dec83c71216226195944c5bf6afb93f2118461ed
Rameshbo2580 <rameshbohora4578@gmail.com>
2026-01-08 22:57:48 +0545

    Taking the all elements from the user for array using Scanner

---
+++ arrayProject.java
@@ -0,0 +1,19 @@
+package Java_Projects;
+import java.util.Scanner;
+public class arrayProject {
+    public static void main(String []arg) {
+      Scanner sc = new Scanner(System.in);
+
+      // ask for the size
+        System.out.println("Enter number of element");
+        int n = sc.nextInt();
+
+        //creating the array
+      int[] x = new int [n] ;
+
+      //counters
+      for(int i = 1 ; i<= x.length;i++){
+
+      }
+    }
+}
