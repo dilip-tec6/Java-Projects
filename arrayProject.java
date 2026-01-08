924215dde479075baf70bab145e5a7bbea56016e
Rameshbo2580 <rameshbohora4578@gmail.com>
2026-01-08 23:08:39 +0545

    Input and analysis is ready

--- arrayProject.java
+++ arrayProject.java
@@ -12,8 +12,20 @@ public class arrayProject {
       int[] x = new int [n] ;
 
       //counters
+        int positive = 0 , negative = 0, zero = 0;
+        int even = 0, odd= 0;
       for(int i = 1 ; i<= x.length;i++){
+          System.out.println("Enter number" + (i+1)+ ":");
+          x[i]=sc.nextInt();
 
+          if (x[i]>0)positive++;
+          else if (x[i]<0) negative++;
+          else zero++;
+
+          if(x[i]%2==0)even++;
+          else odd++;
       }
+      
+        sc.close();
     }
 }
